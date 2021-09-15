71a5a894cd085376650f29513163a7c0353aff70
ElizaKartchner <Eliza.kartchner@gmail.com>
2021-09-15 10:53:11 -0600

    First Commit

--- Initionalization.py
+++ Initionalization.py
@@ -1,13 +1,15 @@
 import csv
-import pandas
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 print("Hello Iris")
 
 fileName = "iris_csv.csv"
 
-with open(fileName, "r") as csvFile:
-    for line in csvFile:
-        print(line)
+df = pd.read_csv(fileName)
 
-print("New update")
+print(df)
 
+list_of_column_names = list(df.columns)
+
+print('List of column names : ',list_of_column_names)
