b69d5b5192bd601ac415ff9f62423e9e3caa37c8
ElizaKartchner <Eliza.kartchner@gmail.com>
2021-09-20 09:29:03 -0600

    Third Commit

--- Initionalization.py
+++ Initionalization.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 
 #Read in the Date from the csv file using Pandas 
 print("Hello Iris")
-fileName = "iris_csv.csv"
+fileName = "DATA/iris_csv.csv"
 df = pd.read_csv(fileName)
 list_of_column_names = list(df.columns)
 print('List of column names : ',list_of_column_names)
@@ -19,7 +19,13 @@ print(df)
 newdf = df[(df["class"] != "Iris-setosa")]
 print(newdf.head())
 print("The number of lines that are not Iris-setosa: " + str(len(newdf)))
+new_list_of_column_names = list(newdf.columns)
+print(new_list_of_column_names)
+#sepallength, sepalwidth, petallength, petalwidth, class 
 
+#Create a modified csv file for the modified data
+newdf.to_csv("DATA/irisModified.csv", index = False)
+modifiedDate = "DATA/irisModified.csv"
 
 #use scikit-learn to perform classification using the Random Forests classifier.
 clf = RandomForestClassifier(random_state=0)
@@ -39,3 +45,14 @@ clf = RandomForestClassifier(random_state=0)
 RandomForestClassifier(random_state=1) # set the random seed using the RANDOM_STATE argument. Use a random seed of 1.
 
 #use clf.score() to score the data
+
+
+#Modify the script so that it creates a TSV file that is clearly named (indicates that it's for the iris data). 
+
+with open("DATA/irisData.tsv", "w") as tsvFile:
+    tsv_writer = csv.writer(tsvFile, delimiter='\t', lineterminator='\n')
+    #tsv_writer.writerow(["Word", "Count"])
+
+#It should have columns that indicate the original row number of each iris flower and the probability that each flower is versicolor (for each of the three classifiers). 
+#It should also have columns that indicate "versicolor" if the probability for a given classifier was >= 0.5 or "virginica" if the probability was < 0.5. 
+#Name these columns descriptively. Each row should indicate which cross-validation fold each flower was used in the test set.
