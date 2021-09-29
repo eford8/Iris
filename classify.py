1ab93cdbbf1a2b8177825c2e53cfabc0ea94e972
Minami <minami.ford@byu.net>
2021-09-29 08:43:42 -0600

    fixed createTSV and cross validate

--- classify.py
+++ classify.py
@@ -32,25 +32,33 @@ def cross_validate(df, labels, clf) :
     iteration = 0
     probs = np.array([]).reshape(0,2)
     auc_roc_scores = []
+    scores = np.array([]).reshape(0,5)
 
     for train_index, test_index in sss.split(X, y):
         iteration += 1
-        #print("TRAIN:", train_index, "TEST:", test_index)
+        print("TRAIN:", train_index, "TEST:", test_index)
         classifier.fit(X[train_index], y[train_index])
         probabilities = classifier.predict_proba(X[test_index])
         probs = np.vstack([probs, probabilities])
-        print("Probabilities")
-        print(probabilities)
+        #print("Probabilities")
+        #print(probabilities)
 
         #Find the rocaucscore
         rocaucscores = roc_auc_score(y, classifier.predict_proba(X)[:, 1])
         auc_roc_scores.append(rocaucscores)
-        print("roc scores")
-        print(rocaucscores)
+        rows, columns = probabilities.shape
+
+        #combine roc_auc score and iteration number to the probabilities 
+        auc = np.zeros((rows,1))
+        auc[:] = rocaucscores
+        combined = np.insert(probabilities, 0, [iteration], axis=1)
+        combined = np.append(combined, auc, axis=1)
+        combined = np.insert(combined, 1, test_index, axis=1)
+        scores = np.vstack([scores,combined])
+
     
-    print(probs.shape)
-    print(auc_roc_scores)
-    return probs, auc_roc_scores
+    print(scores)
+    return scores
     scores = []
     #for i in range(iterations) :
         #params = clf[1]
@@ -95,13 +103,13 @@ predictColumn = df[:, 5]
 
 
 for classifier in CLASSIFIERS:
-    scores, auc_scores = cross_validate(data, predictColumn, classifier)
-    for score in scores:
+    for score in cross_validate(data, predictColumn, classifier):
         classifier_name = str(classifier[0]).split("'")[1].split(".")[-1].replace("Classifier", "")
-        results.append([classifier_name, "labels", str(score)])
+        results.append([classifier_name, score[0], score[1], score[2], score[3], score[4]])
 
 print("scores")
 print(results)
+## results are [classifier, iteration number, og row number, probability not virginica, probability virginica, roc score]
 
 
 #Modify the script so that it creates a TSV file that is clearly named (indicates that it's for the iris data). 
@@ -111,7 +119,18 @@ def createTSV(results):
         #It should have columns that indicate the original row number of each iris flower and the probability that each flower is versicolor (for each of the three classifiers). 
         #It should also have columns that indicate "versicolor" if the probability for a given classifier was >= 0.5 or "virginica" if the probability was < 0.5. 
         #Name these columns descriptively. Each row should indicate which cross-validation fold each flower was used in the test set.
-        tsvFile.write("ogRowNum\ttypeClassifier\tprbVersi(RandomForest, LogReg, KNeighbor)\tversi/virgincia\tcross-validationFold\n")
+        tsvFile.write("ogRowNum\ttClassifier\tprobability of versicolor\tprediction\tcross-validationFold\n")
+        rownum = 1
+        for prediction in results:
+            # checks probability of versicolor and makes prediction 
+            predict = ""
+            if(prediction[4] >= 0.5) :
+                predict = "versicolor"
+            else:
+                predict = "virginica"
+    
+            tsvFile.write('\t'.join([str(int(prediction[2])), str(prediction[0]), str(prediction[4]), predict, str(int(prediction[1]))]) + '\n')
+            #tsvFile.write(int(results[2]) + '\t' + results[0] + '\t' + results[4] + '\t' + "prediction" + '\t' + int(results[1]) + '\n')
         #example: 3 RandomForest 0.6    Versicolor  3
         #example: 5 LogReg 0.2 Virgincia 1
 
