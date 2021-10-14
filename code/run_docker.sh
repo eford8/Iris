c7affdef24e97e891535f8b1f40801f62a003a5c
eford8 <emi@byu.net>
2021-10-14 11:04:17 -0600

    fixing dockerfiles

--- code/run_docker.sh
+++ code/run_docker.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-image=https://github.com/eford8/Iris.git
+image=https://github.com/eford8/Iris.git#main
 
-docker build $image 
+docker build -f code/Dockerfile $image
 
 #mkdir -p data/simulated_expression data/mnist data/bladderbatch data/gse37199 data/tcga
 #mkdir -p $(pwd)/../metrics
-mkdir -p $(pwd)/../figures
-printf "\033[0;32mHere\033[0m\n"
+#mkdir -p $(pwd)/../figures
+#printf "\033[0;32mHere\033[0m\n"
 #chmod 777 $(pwd)/../metrics
-chmod 777 $(pwd)/../figures
-printf "\033[0;32mHere2\033[0m\n"
+#chmod 777 $(pwd)/../figures
+#printf "\033[0;32mHere2\033[0m\n"
 #docker run -i -t --rm \
-docker run -i --rm \
-  --user $(id -u):$(id -g) \
-  -v $(pwd)/data:/data \
-  -v $(pwd)/../metrics:/output/metrics \
-  -v $(pwd)/../figures:/output/figures \
-printf "\033[0;32mHere3\033[0m\n"
-  $image
+#docker run -i --rm \
+#  --user $(id -u):$(id -g) \
+#  -v $(pwd)/data:/data \
+#  -v $(pwd)/../metrics:/output/metrics \
+#  -v $(pwd)/../figures:/output/figures \
+#printf "\033[0;32mHere3\033[0m\n"
+#  $image
