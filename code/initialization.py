565f324c54ea45558bf67aff1f2f172cab9c995e
ElizaKartchner <Eliza.kartchner@gmail.com>
2021-11-03 08:47:13 -0600

    change pathway

--- code/initialization.py
+++ code/initialization.py
@@ -7,7 +7,7 @@ def irisDataInitialization () :
 
     #Read in the Date from the csv file using Pandas 
     print("Initializing Iris Data\n")
-    fileName = "iris.csv"
+    fileName = "data/iris.csv"
     df = pd.read_csv(fileName)
     list_of_column_names = list(df.columns)
     print('List of column names : ',list_of_column_names)
@@ -37,8 +37,8 @@ def irisDataInitialization () :
     #sepallength, sepalwidth, petallength, petalwidth, class, lables 
 
     #Create a modified csv file for the modified data
-    newdf.to_csv("irisModified.csv", index = False)
-    modifiedDate = "irisModified.csv"
+    newdf.to_csv("data/irisModified.csv", index = False)
+    #modifiedDate = "data/irisModified.csv"
 
 #irisDataInitialization()
 
@@ -112,16 +112,16 @@ def gameteEpistasisInitialization() :
 def generalInitialization(dataType) :
     #Read in the Date from the tsv file using Pandas 
     print("Initializing " + dataType + "\n")
-    fileName = dataType + ".tsv"
+    fileName = "data/" + dataType + ".tsv"
     df = pd.read_csv(fileName, sep='\t', header=0) #dataframe
     list_of_column_names = list(df.columns)
     print('List of column names : ', list_of_column_names)
     print(df.head())
 
     #Create a modified csv file for the modified data
-    newFileName = dataType + ".csv"
+    newFileName = "data/" + dataType + "Modified.csv"
     df.to_csv(newFileName, index = False)
-    modifiedDate = newFileName
+    #modifiedDate = newFileName
 
 generalInitialization("breast")
 generalInitialization("horse_colic")
