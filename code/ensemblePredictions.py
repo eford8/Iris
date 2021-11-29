7bd1e704bb66c3f95c3af8110059930e33644392
ElizaKartchner <Eliza.kartchner@gmail.com>
2021-11-29 08:32:22 -0700

    Update tsv file

--- code/ensemblePredictions.py
+++ code/ensemblePredictions.py
@@ -4,6 +4,7 @@ import statistics
 
 #################
 ## This file take the files created from classificationAccuracy.py and finds the avgPredition, majorityVote, and maxProb
+## It is a basic emsemble 
 #################
 
 resultsFile = 'results/' + sys.argv[1] + 'Classifications.tsv'
@@ -78,7 +79,7 @@ listavg = avgPredition()
 listmajority = majorityVote()
 listmax = maxProb()
 
-
+#This is the old code for creating the tsv file
 #create a tsv file
 with open(outfile, "w") as tsvFile:
     tsvFile.write("OriginalRow\tTarget\tIteration\tAvgPrediction\tMajorityVote\tExtremeProb\n")
@@ -87,22 +88,37 @@ with open(outfile, "w") as tsvFile:
         tsvFile.write(str(row["OriginalRow"]) + '\t' + str(row["Target"]) + '\t' + str(row["Iteration"]) + "\t" +  str(listavg[x]) + '\t' + str(listmajority[x]) + '\t' + str(listmax[x]) + '\n')
 
 
-##New TSV file 
+##New TSV file as to how Dr. Piccolo wanted it to be 
 
 print("Creating TSV file...")
 with open(outfile, "w") as tsvFile:
     print("writing to " + outfile)
     tsvFile.write("OriginalRow\tTarget\tIteration\tClassifier\tPredictionType\tPredictionScore\tPrediction\n")
-    ##PredictionScore can be the .85 or whatever the percentage is
-    ##Prediction can be a 1 or a 0 (or 2) for what the prediction would be 
-    ## Fix the Prediction part 
+   
     for x in range(elementsPerClassifier) :
         row = dataResults.iloc[x]
-        tsvFile.write(str(row["OriginalRow"])+'\t'+str(row["Target"])+'\t'+str(row["Iteration"])+"\t"+ str("BasicEnsemble")+'\t'+str("AvgPrediction")+'\t'+ str(listavg[x]) +'\n')
-        tsvFile.write(str(row["OriginalRow"])+'\t'+str(row["Target"])+'\t'+str(row["Iteration"])+"\t"+ str("BasicEnsemble")+'\t'+str("MajorityVote")+'\t'+ str(listmajority[x]) +'\n')
-        tsvFile.write(str(row["OriginalRow"])+'\t'+str(row["Target"])+'\t'+str(row["Iteration"])+"\t"+ str("BasicEnsemble")+'\t'+str("ExtremeProb")+'\t'+ str(listmax[x]) +'\n')
+
+        avgPreditionClass = 0
+        majorityPreditionClass = 0
+        maxPreditionClass = 0
+        #Assigns the class prediction based on the prediction score 
+        if listavg[x] >= 0.5:
+            avgPreditionClass = class1
+        else:
+            avgPreditionClass = class2
+        if listmajority[x] >= 0.5:
+            majorityPreditionClass = class1
+        else:
+            majorityPreditionClass = class2
+        if listmax[x] >= 0.5:
+            maxPreditionClass = class1
+        else:
+            maxPreditionClass = class2
+        
+        tsvFile.write(str(row["OriginalRow"])+'\t'+str(row["Target"])+'\t'+str(row["Iteration"])+"\t"+ str("BasicEnsemble")+'\t'+str("AvgPrediction")+'\t'+str(listavg[x])+'\t'+str(avgPreditionClass)+'\n')
+        tsvFile.write(str(row["OriginalRow"])+'\t'+str(row["Target"])+'\t'+str(row["Iteration"])+"\t"+ str("BasicEnsemble")+'\t'+str("MajorityVote")+'\t'+str(listmajority[x])+'\t'+str(majorityPreditionClass)+'\n')
+        tsvFile.write(str(row["OriginalRow"])+'\t'+str(row["Target"])+'\t'+str(row["Iteration"])+"\t"+ str("BasicEnsemble")+'\t'+str("ExtremeProb")+'\t'+str(listmax[x])+'\t'+str(maxPreditionClass)+'\n')
         
     
 
-   
-
+ 
