1c51bde4c294c3777af71a1fccba7567f27c8254
ElizaKartchner <Eliza.kartchner@gmail.com>
2021-12-06 10:45:59 -0700

    Adding AUPRC

--- code/metrics.py
+++ code/metrics.py
@@ -7,6 +7,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score    #also used to calculate f1_weighted 
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_recall_curve #AUPRC
 
 ####################
 ## This Python script parses the tsv file created from "classify.py" and calculates various accuracy scorces using
@@ -33,7 +34,7 @@ def metrics ():
     numIterations = 5 + 1 #fix this later
 
     with open(outfile, "w") as tsvFile:
-        tsvFile.write("Classifier\tIteration\tAccuracy\tf1_score\tf1_weighted\taverage_precision\troc_auc\n")
+        tsvFile.write("Classifier\tIteration\tAccuracy\tf1_score\tf1_weighted\taverage_precision\troc_auc\tprecision\trecall\tthresholds\n")
 
         for classifier in listOfClassifiers:
             for iteration in range(1, numIterations):
@@ -54,42 +55,15 @@ def metrics ():
                 f1_weighted = f1_score(y_target, y_pred, average='weighted')
                 average_precision = average_precision_score(y_target, y_pred)
                 rocAucScore = roc_auc_score(y_target, y_pred)
+                #implementing AUPRC
+                precision, recall, thresholds = precision_recall_curve( y_target, y_pred) #AUPRC
+                print(precision)
+                print(recall)
+                print(thresholds)
 
                 ### writing the information to the tsv file
                 print("The metrics for " + classifier + " for iteration " + str(iteration) + " are: " )
-                tsvFile.write('\t'.join([classifier, str(iteration), str(accuracy), str(f1Score), str(f1_weighted), str(average_precision), str(rocAucScore)])+ '\n')
-
-
-    # for x in range(elementsPerClassifier):
-    #     rowNum = dataResults.iloc[x, 0]
-    #     iteration = dataResults.iloc[x, 4]
-        
-    #     for i in range(len(dataResults)):
-    #         row = dataResults.iloc[i]
-    #         y_pred = []
-    #         y_true = []
-    #         if(str(row['OriginalRow']) == str(rowNum)) : # change to original
-    #             if(str(row['Iteration']) == str(iteration)) :
-                    
-    #                 ######
-    #                 ## for everything except the roc_score, we just need a way to create two lists/arrays. 
-    #                 ## one called y_pred with the predictions
-    #                 ## one called y_true with what the orignal data says
-    #                 ## then we can easily get all the accuracy metrics
-    #                 ######
-
-    #                 y_pred = [0, 1, 0]
-    #                 y_true = [0, 0, 1]
-                
-    #                 accuracy = accuracy_score(y_true, y_pred)
-    #                 f1Score = f1_score(y_true, y_pred, average='binary')
-    #                 f1_weighted = f1_score(y_true, y_pred, average='weighted')
-    #                 average_precision = average_precision_score(y_true, y_pred)
-    #                 rocAucScore = roc_auc_score(y_true, y_pred)
-
-    #                 ### writing the information to the tsv file
-    #                 print("The metrics for " + classifier + " for iteration " + iteration + " are: " )
-    #                 tsvFile.write('\t'.join([classifier + str(iteration) + str(accuracy) + str(f1Score) + str(f1_weighted) + str(average_precision)]) + str(rocAucScore) + '\n')
+                tsvFile.write('\t'.join([classifier, str(iteration), str(accuracy), str(f1Score), str(f1_weighted), str(average_precision), str(rocAucScore), str(precision), str(recall), str(thresholds)])+ '\n')
 
 metrics()
 
