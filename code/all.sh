bada379063b89fceda1c1d958a6db6b47caab1cc
ElizaKartchner <Eliza.kartchner@gmail.com>
2022-01-10 08:53:58 -0700

    Update metrics arguments

--- code/all.sh
+++ code/all.sh
@@ -17,8 +17,15 @@ python ensemblePredictions.py "Gametes_Epistasis" "1" "0"
 python ensemblePredictions.py "magic" "1" "0"
 
 
-python metrics.py "iris" 
-python metrics.py "breast"
-python metrics.py "horse_colic"
-python metrics.py "Gametes_Epistasis"
-python metrics.py "magic"
+python metrics.py "iris" "Classifications"
+python metrics.py "breast" "Classifications"
+python metrics.py "horse_colic" "Classifications"
+python metrics.py "Gametes_Epistasis" "Classifications"
+python metrics.py "magic" "Classifications"
+
+
+python metrics.py "iris" "EnsemblePredictions"
+python metrics.py "breast" "EnsemblePredictions"
+python metrics.py "horse_colic" "EnsemblePredictions"
+python metrics.py "Gametes_Epistasis" "EnsemblePredictions"
+python metrics.py "magic" "EnsemblePredictions"
