b14bf64571d36d46857f57a86f98cbddaef194df
ElizaKartchner <Eliza.kartchner@gmail.com>
2022-01-13 14:47:10 -0700

    add predictionType

--- code/metrics.py
+++ code/metrics.py
@@ -9,14 +9,14 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
 
 ####################
-## This Python script parses the tsv file created from "classify.py" and calculates various accuracy scores using
+## This Python script parses the tsv file created from "classify.py" or from ensemblePredictions.py and calculates various accuracy scores using
 ## sklearn.metrics. It does this for all the algorithms used and for each iteration.
 ## We can see how frequently the predictions from our algorithms coincide with the actual species. 
 ####################
 
 def metrics ():
     print("Calculating metrics for " + sys.argv[1] + sys.argv[2] + " dataset\n")
-    resultsFile = 'results/' + sys.argv[1] + sys.argv[2] + '.tsv' #the file created from classify.py
+    resultsFile = 'results/' + sys.argv[1] + sys.argv[2] + '.tsv' #the file created from classify.py or from ensemblePredictions.py
     originalDataFile = 'data/' + sys.argv[1] + 'Modified.csv'
     outFile = 'results/' + sys.argv[1] + sys.argv[2] + 'Metrics.tsv'
     
@@ -28,7 +28,7 @@ def metrics ():
     numIterations = dataResults["Iteration"].max() + 1
 
     with open(outFile, "w") as tsvFile:
-        tsvFile.write("DataName\tClassifier\tIteration\tAccuracy\tf1_score\tf1_weighted\taverage_precision\troc_auc\n")
+        tsvFile.write("DataName\tClassifier\tIteration\tPredictionType\tAccuracy\tf1_score\tf1_weighted\taverage_precision\troc_auc\n")
 
         for classifier in listOfClassifiers:
             for iteration in range(1, numIterations):
@@ -48,8 +48,10 @@ def metrics ():
                 averagePrecision = average_precision_score(y_target, y_pred)
                 rocAucScore = roc_auc_score(y_target, y_pred)
 
+                predictionType = dataResults.iat[1, "PredictionType"]
+                
                 ### writing the information to the tsv file
                 print("Writing the metrics for " + classifier + " for iteration " + str(iteration))
-                tsvFile.write('\t'.join([str(sys.argv[1]), classifier, str(iteration), str(accuracy), str(f1Score), str(f1Weighted), str(averagePrecision), str(rocAucScore)]) + '\n')
+                tsvFile.write('\t'.join([str(sys.argv[1]), classifier, str(iteration), str(predictionType), str(accuracy), str(f1Score), str(f1Weighted), str(averagePrecision), str(rocAucScore)]) + '\n')
 
 metrics()
