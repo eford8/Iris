9356320e72fc9157a9d22c579339fcbe87e56b00
Eliza Kartchner <eliza.kartchner@gmail.com>
2022-01-21 14:21:47 -0700

    updated for new file

--- code/all.sh
+++ code/all.sh
@@ -1,31 +1,36 @@
 #! /bin/bash
 
-python initialization.py
+#python initialization.py
 
-python classify.py "iris" "Iris-versicolor" "Iris-virginica"
-python classify.py "iris" "1" "0"
-python classify.py "breast" "1" "0"
-python classify.py "horse_colic" "2" "1"
-python classify.py "Gametes_Epistasis" "1" "0"
-python classify.py "magic" "1" "0"
-
-
-python ensemblePredictions.py "iris" "1" "0"
-python ensemblePredictions.py "breast" "1" "0"
-python ensemblePredictions.py "horse_colic" "2" "1"
-python ensemblePredictions.py "Gametes_Epistasis" "1" "0"
-python ensemblePredictions.py "magic" "1" "0"
+#python classify.py "iris" "Iris-versicolor" "Iris-virginica"
+#python classify.py "iris" "1" "0"
+#python classify.py "breast" "1" "0"
+#python classify.py "horse_colic" "2" "1"
+#python classify.py "Gametes_Epistasis" "1" "0"
+#python classify.py "magic" "1" "0"
 
 
-python metrics.py "iris" "Classifications"
-python metrics.py "breast" "Classifications"
-python metrics.py "horse_colic" "Classifications"
-python metrics.py "Gametes_Epistasis" "Classifications"
-python metrics.py "magic" "Classifications"
+#python ensemblePredictions.py "iris" "1" "0"
+#python ensemblePredictions.py "breast" "1" "0"
+#python ensemblePredictions.py "horse_colic" "2" "1"
+#python ensemblePredictions.py "Gametes_Epistasis" "1" "0"
+#python ensemblePredictions.py "magic" "1" "0"
 
 
-python metrics.py "iris" "EnsemblePredictions"
-python metrics.py "breast" "EnsemblePredictions"
-python metrics.py "horse_colic" "EnsemblePredictions"
-python metrics.py "Gametes_Epistasis" "EnsemblePredictions"
-python metrics.py "magic" "EnsemblePredictions"
+#python metrics.py "iris" "Classifications"
+#python metrics.py "breast" "Classifications"
+#python metrics.py "horse_colic" "Classifications"
+#python metrics.py "Gametes_Epistasis" "Classifications"
+#python metrics.py "magic" "Classifications"
+
+
+#python metrics.py "iris" "EnsemblePredictions"
+#python metrics.py "breast" "EnsemblePredictions"
+#python metrics.py "horse_colic" "EnsemblePredictions"
+#python metrics.py "Gametes_Epistasis" "EnsemblePredictions"
+#python metrics.py "magic" "EnsemblePredictions"
+
+python addWeights.py "iris" "Classifications"
+
+
+
